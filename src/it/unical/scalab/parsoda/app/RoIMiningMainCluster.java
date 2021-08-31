afe804dd92572630053d41c9a08fe6514bad89c1
loris <lorisbel@gmail.com>
2021-08-31 11:19:01 +0200

    fixed app

--- src/it/unical/scalab/parsoda/app/RoIMiningMainCluster.java
+++ src/it/unical/scalab/parsoda/app/RoIMiningMainCluster.java
@@ -10,29 +10,30 @@ import it.unical.scalab.parsoda.visualization.RoIsToKML;
 
 public class RoIMiningMainCluster {
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static void main(String[] args) {
-		String colosseumSynonyms = "colosse:colis:collis:collos:Amphiteatrum Flavium:Amphitheatrum Flavium:An Colasaem:Coliseo:Coliseo:Coliseo de Roma:Coliseo de Roma:Coliseu de Roma:Coliseum:Coliseum:Coliseum:Coliseus:Colloseum:Coloseu:Colosseo:Colosseo:Colosseo:Colosseu:Colosseum:Colosseum:Colosseum:Colosseum:Colosseum:Colosseum:Colosseum:Colosseum:Colosseum:Colosseum:Culusseu:Kolezyum:Koliseoa:Kolize:Kolizejs:Kolizey:Kolizey:Koliziejus:Kolosej:Kolosej:Koloseo:Koloseo:Koloseum:Koloseum:Koloseum:Koloseum:Koloseum:Koloseum:Koloseum:Koloseumi:Kolosseum:Kolosseum:Kolosseum:Kolosseum";
-		SocialDataApp app = new SocialDataApp("RoI Mining - Colosseum");
-		app.setOutputBasePath("/user/loris/outputApp");
-		app.setHDFS("172.20.0.2","9000");
-		app.setNumReducer(1);
-		Class[] cFunctions = { FileReaderCrawler.class };
-		String[] cParams = { "-i /user/loris/Colosseum500m.json" };
-		app.setCrawlers(cFunctions, cParams);
-		Class[] fFunctions = { IsGeotagged.class, ContainsKeywords.class };
-		String[] fParams = { " ", "-separator : -keywords " + colosseumSynonyms };
-		app.setFilters(fFunctions, fParams);
-		Class rFunction = ReduceByCoordinates.class;
-		String rParams = "-t 5";
-		app.setReduceFunction(rFunction, rParams);
-		Class aFunction = ExtractRoIs.class;
-		String aParams = "-minPts 150 -eps 30";
-		app.setAnalysisFunction(aFunction, aParams);
-		Class vFunction = RoIsToKML.class;
-		String vParams = null;
-		app.setVisualizationFunction(vFunction, vParams);		
-		app.execute();
-	}
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public static void main(String[] args) {
+        String colosseumSynonyms = "colosse:colis:collis:collos:Amphiteatrum Flavium:Amphitheatrum Flavium:An Colasaem:Coliseo:Coliseo:Coliseo de Roma:Coliseo de Roma:Coliseu de Roma:Coliseum:Coliseum:Coliseum:Coliseus:Colloseum:Coloseu:Colosseo:Colosseo:Colosseo:Colosseu:Colosseum:Colosseum:Colosseum:Colosseum:Colosseum:Colosseum:Colosseum:Colosseum:Colosseum:Colosseum:Culusseu:Kolezyum:Koliseoa:Kolize:Kolizejs:Kolizey:Kolizey:Koliziejus:Kolosej:Kolosej:Koloseo:Koloseo:Koloseum:Koloseum:Koloseum:Koloseum:Koloseum:Koloseum:Koloseum:Koloseumi:Kolosseum:Kolosseum:Kolosseum:Kolosseum";
+        SocialDataApp app = new SocialDataApp("RoI Mining - Colosseum");
+        app.setOutputBasePath(args[0].trim() + "/outputApp");
+        app.setHDFS("namenode", "9000");
+        app.setNumReducer(1);
+
+        Class[] cFunctions = {FileReaderCrawler.class};
+        String[] cParams = {"-i " + args[0].trim() + "/Colosseum500m.json"};
+        app.setCrawlers(cFunctions, cParams);
+        Class[] fFunctions = {IsGeotagged.class, ContainsKeywords.class};
+        String[] fParams = {" ", "-separator : -keywords " + colosseumSynonyms};
+        app.setFilters(fFunctions, fParams);
+        Class rFunction = ReduceByCoordinates.class;
+        String rParams = "-t 5";
+        app.setReduceFunction(rFunction, rParams);
+        Class aFunction = ExtractRoIs.class;
+        String aParams = "-minPts 150 -eps 30";
+        app.setAnalysisFunction(aFunction, aParams);
+        Class vFunction = RoIsToKML.class;
+        String vParams = null;
+        app.setVisualizationFunction(vFunction, vParams);
+        app.execute();
+    }
 
 }
