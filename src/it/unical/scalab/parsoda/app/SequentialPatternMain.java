e9f46f18d1287aeabe674a015ad1265bd31e09bb
loris <lorisbel@gmail.com>
2021-10-13 15:56:39 +0200

    fixed app

--- src/it/unical/scalab/parsoda/app/SequentialPatternMain.java
+++ src/it/unical/scalab/parsoda/app/SequentialPatternMain.java
@@ -12,30 +12,41 @@ public class SequentialPatternMain {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
 		SocialDataApp app = new SocialDataApp("Sequential Pattern Mining - City of Rome");
+
+		// DATA ACQUISITION
 		String[] cFiles = { "resources/RomeRoIs.kml" };
-		app.setDistributedCacheFiles(cFiles);
-		app.setLocatFileSystem();
+		app.setDistributedCacheFiles(cFiles); // DISTRIBUTE AN ARRAY OF FILES TO ALL MAP/REDUCE NODES
+		app.setLocatFileSystem(); // USE THE LOCAL FILESYSTEM (FOR TESTING PURPOSE)
 		Class[] cFunctions = { FileReaderCrawler.class };
-//		String[] cParams = { "-i resources/FlickrRome2017.json" };
 		String[] cParams = { "-i resources/Colosseum500m.json" };
 		app.setCrawlers(cFunctions, cParams);
+
+		// DATA FILTERING
 		Class[] fFunctions = { IsGeotagged.class, IsInPlace.class };
 		String[] fParams = { " ", "-lng 12.492 -lat 41.890 -radius 10000" };
 		app.setFilters(fFunctions, fParams);
+
+		// DATA MAPPING
 		Class[] mFunctions = { FindPoI.class };
 		String[] mParams = null;
-
 		app.setMapFunctions(mFunctions, mParams);
+
+		// DATA PARTITIONING
 		String groupKey = "USER.USERID";
 		String sortKey = "DATETIME";
 		app.setPartitioningKeys(groupKey, sortKey);
 
+		// DATA REDUCTION
 		Class rFunction = ReduceByTrajectories.class;
 		String rParams = "-t 5";
-		app.setReduceFunction(rFunction, rParams);		
+		app.setReduceFunction(rFunction, rParams);
+
+		// DATA ANALYSIS
 		Class aFunction = PrefixSpan.class;
 		String aParams = "-maxPatternLength 5 -minSupport 0.005";
 		app.setAnalysisFunction(aFunction, aParams);
+
+		// DATA VISUALIZATION
 		Class vFunction = SortPrefixSpanBy.class;
 		String vParams = "-k support -d DESC";
 		app.setVisualizationFunction(vFunction, vParams);
